554d995127666fdc75c2447081b0fc0670692db3
go <goito.722@icloud.com>
2024-02-13 23:15:07 +0900

    delete notionarticle.ts program

--- pages/api/NotionArticle.ts
+++ pages/api/NotionArticle.ts
@@ -4,32 +4,6 @@ import axios from "axios";
 import dotenv from "dotenv"
 // pages/api/notionData.js
 
-const handler = async (req:NextApiRequest, res:NextApiResponse) => {
-    const NOTION_API_URL = 'https://api.notion.com/v1/';
-    const DATABASE_ID = '3bf9e0082abe45d49cd8d3260be0ca56';
-    const INTEGRATION_TOKEN = process.env.NOTION_TOKEN;
-  
-    try {
-      const response = await fetch(`${NOTION_API_URL}databases/${DATABASE_ID}/query`, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${INTEGRATION_TOKEN}`,
-          'Content-Type': 'application/json',
-          'Notion-Version': '2022-06-28'
-        }
-      });
-  
-      if (!response.ok) {
-        throw new Error('Notion API request failed');
-      }
-  
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-      console.error('Error fetching data from Notion API:', error);
-      res.status(500).json({ error: 'Failed to fetch data from Notion API' });
-    }
-  }
-  
 
-export default handler;
+
+export default ;
