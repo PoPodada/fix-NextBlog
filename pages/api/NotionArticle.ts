cb672779c0e2d8c5448688eed0ac12bfda68d62b
go <goito.722@icloud.com>
2024-02-16 00:26:08 +0900

    delete api file

--- pages/api/NotionArticle.ts
+++
@@ -1,9 +0,0 @@
-
-import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from "axios";
-import dotenv from "dotenv"
-// pages/api/notionData.js
-
-
-
-export default ;
--- pages/components/MainPage.tsx
+++ pages/components/MainPage.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 
 export const MainPage = () => {
-   
+    
+   const onClicksetColor
     return(
         <>
             <button onClick={}></button>
